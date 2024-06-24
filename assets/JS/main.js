a54cbe2159aa2e19ae42c6c0c58a66f243e2c38f
FelipeSanches10 <fvieirasanches@gmail.com>
2024-06-24 00:40:49 -0300

    Atualização

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -6,10 +6,48 @@ btn.addEventListener("click", function() {
 
 function myFunction() {
   var x = document.getElementById("myLinks");
-  
+
   if (x.style.display === "block") {
     x.style.display = "none";
   } else {
     x.style.display = "block"
   }
 }
+
+document.querySelectorAll('.btn-card').forEach(button => {
+  button.addEventListener('click', event => {
+      const itemId = event.target.getAttribute('data-id');
+      const itemPrice = parseFloat(event.target.getAttribute('data-price'));
+      let quantity = parseInt(prompt("Quantas unidades você deseja adicionar?"), 10);
+
+      if (!isNaN(quantity) && quantity > 0) {
+          const totalPrice = itemPrice * quantity;
+          document.querySelector(`.span-price[data-id="${itemId}"]`).innerText = `R$${totalPrice.toFixed(2)}`;
+          
+    Swal.fire({
+      title: "Perfeito",
+      text: "Seu pedido foi confirmado",
+      icon: "success"
+});
+
+      } else {
+        Swal.fire({
+          icon: "error",
+          title: "Ops...",
+          text: "Por favor, insira um número válido de unidades."
+        });
+      }
+
+     
+  });
+});
+
+
+// Swal.fire({
+//   title: "Perfeito",
+//   text: "Seu pedido foi confirmado",
+//   icon: "success"
+// });
+
+
+
